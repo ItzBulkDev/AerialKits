3044aae88f4013e2d0ba3ec7299eb04c9a3b3060
Bulk Developer <jaydenpatrick1110@gmail.com>
2016-04-10 12:57:51 -0400

    Upload this to web-server to detect user's rank
    Make sure you have a database, then change the $servername, $username, $password, and $dbname to your database's information

---
+++ check.php
@@ -0,0 +1,24 @@
+<?php
+//UPLOAD AND CONFIGURE FILE TO WEB SERVER
+
+$servername = "localhost";
+$username = "DATABASE_USERNAME";
+$password = "DATABASE_PASSWORD";
+$dbname = "DATABASE_NAME";
+
+$name = $_GET['name'];
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT rank FROM Ranks WHERE name='$name'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    $rank = $row['rank'];
+    echo $rank;
+    }
+    }else{
+    echo "No Rank";
+    }
+    
+    ?>
